c1c7a5e3f1e378128a0a0312020a3524101cd8d0
Maksim Kalasouski <66016998+maxim-kolosovskii@users.noreply.github.com>
2021-12-28 12:48:43 +0300

    Add files via upload
    lazy loading for internal image

--- js/lazy.js
+++ js/lazy.js
@@ -46,3 +46,37 @@ document.addEventListener("DOMContentLoaded", function() {
         window.addEventListener("orientationChange", lazyload);
     }
 })
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyloadImages = document.querySelectorAll("img.lazy-img");
+    var lazyloadThrottleTimeout;
+
+    function lazyload () {
+        if(lazyloadThrottleTimeout) {
+            clearTimeout(lazyloadThrottleTimeout);
+        }
+
+        lazyloadThrottleTimeout = setTimeout(function() {
+            var scrollTop = window.pageYOffset;
+            lazyloadImages.forEach(function(img) {
+                if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy-img');
+                }
+            });
+            if(lazyloadImages.length == 0) {
+                document.removeEventListener("scroll", lazyload);
+                window.removeEventListener("resize", lazyload);
+                window.removeEventListener("orientationChange", lazyload);
+            }
+        }, 20);
+    }
+
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+});
